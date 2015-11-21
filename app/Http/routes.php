dac783f26900ef04cc532182ddca6eb12f4d858c
zhangxu <xuzhang@thoughtworks.com>
2015-11-21 15:39:34 +0800

    change database file to right location

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,16 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+get('guestbook', function() {
+	return view('guestbook');
+});
 
-Route::get('/', function () {
-    return view('welcome');
+// API
+
+get('api/messages', function() {
+	return App\Message::all();
+});
+
+post('api/messages', function() {
+	App\Message::create(Request::all());
 });
