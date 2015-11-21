bd8eb994f5adc9398618d6d6d31be788b1407c52
zhangxu <xuzhang@thoughtworks.com>
2015-11-21 16:02:09 +0800

    use tinker to generate fake data

--- resources/views/guestbook.blade.php
+++ resources/views/guestbook.blade.php
@@ -5,6 +5,19 @@
 	<title>Guestbook</title>
 </head>
 <body>
-	
+	<div id="guestbook">
+		<article v-repeat="messages">
+			
+			<h3>@{{ name }}</h3>
+			
+			<div class="body">
+				@{{ message }}	
+			</div>
+
+		</article>	
+	</div>
+
+	<script type="text/javascript" src="/js/vendor.js"></script>
+	<script type="text/javascript" src="/js/guestbook.js"></script>
 </body>
 </html>
